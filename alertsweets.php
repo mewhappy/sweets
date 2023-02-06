1602e8f4f31d27aac54c458d1c9c868c07278a13
Tachchapan.p <42629064+mewhappy@users.noreply.github.com>
2023-02-06 15:00:25 +0700

    sweetalert2
    sweet alert

---
+++ alertsweets.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+</head>
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+ $(function () {
+Swal.fire({
+            icon: 'error',
+            title: 'Aww!!',
+            text: 'Not 0!',
+        })
+        return false;
+    })
+        </script>
+    </body>
+</html>
